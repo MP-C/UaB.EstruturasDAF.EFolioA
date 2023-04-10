46ed833921fc87fe47ec86fef89332414221e9bd
capelam <mario.capela@stpierre-bru.be>
2023-04-10 23:43:30 +0200

    Correção de várias funções : 10, 11, 12 e 13. Alguns testes local OK! muito ainda não. insert negativos não faz

--- isll.cpp
+++ isll.cpp
@@ -96,7 +96,8 @@ void ISll::print(string cmd) {
                 cout << atual->item << " ";// Imprime o valor atual começando no nó inicial
                 atual = atual->next;       // Move o ponteiro para o próximo nó da lista
             }
-            cout << endl;                  // Quebra de linha para separar a impressão da lista de outras saídas
+            //atual->next=NULL;
+            cout <<"\n"<<endl;                  // Quebra de linha para separar a impressão da lista de outras saídas
         };
     };
 }
@@ -132,11 +133,12 @@ void ISll::delete_end(string cmd) {
         //int elemento = head->item;
         INode *temporario = head;              // Criar nó temporário, que é usado para repor o valor assim que o último é apagado
         if (head == tail) {                    // Se a lista tem apenas um elemento, remove e atualiza head e tail
-            delete head;                       // Remove head
-            delete tail;                       // Remove tail
+            //delete head;                       // Remove head
+            //delete tail;                       // Remove tail
             head = tail = nullptr;             // Atualiza head e tail
             n = 0;                             // Atualiza contador
         }else{
+            /*
             // Se a lista tem mais de um elemento, percorre a lista até chegar ao penúltimo elemento
             while(temporario->next != tail) {
                 temporario = temporario->next;
@@ -147,15 +149,25 @@ void ISll::delete_end(string cmd) {
             tail->next = NULL;                 // Atualiza tail
             delete temporario;                 // Apaga o temporário para libertar memória
             n--;                               // Atualiza contador
+             */
+            INode *p = head;                // Cria um ponteiro "p" que aponta para o primeiro elemento da lista (head)
+            INode *q = NULL;                // Cria um ponteiro "q" que apontará para o elemento anterior ao último
+            while (p->next != NULL) {       // Percorre a lista enquanto o nó seguinte não for nulo
+                q = p;                      // Guarda o nó anterior
+                p = p->next;                // Move o ponteiro para o próximo nó da lista
+            }
+            q->next = NULL;                 // Atribui NULL ao ponteiro next do penúltimo elemento
+            delete p;                       // Elimina o último elemento da lista
+            n--;                            // Decrementa o número de elementos da lista
         }
     }
 }
 
 // 8. Imprimir o número total de itens na lista //TODO: OK!!
 void ISll::dim(string cmd) {
-    if(verificarListaVazia(cmd) == false){ // Verifica se a lista está vazia
+    //if(verificarListaVazia(cmd) == false){ // Verifica se a lista está vazia
         cout << "Lista tem "<<  n << " itens\n" << endl; // Imprime o contador atualizado da quantidade de nós contados na lista
-    }
+    //}
 }
 
 // 9. Remover os nós da lista //TODO: OK!!
@@ -191,7 +203,7 @@ void ISll::find(string cmd, string argumentos) {
 // 11. Procurar a primeira ocorrência do maior item na lista e imprimir posição
 void ISll::find_max(string cmd, string argumento){ //TODO devia estar a funcionar..mas não está! as vezes bate certo...aaaaaaahhhhhh!
     if(verificarListaVazia(cmd) == false) {
-        int pos = 0;
+        int pos = 1;
         int max_val = head->item; // começa com o primeiro valor
         INode* atual = head->next;
         while (atual != NULL) {
@@ -200,6 +212,7 @@ void ISll::find_max(string cmd, string argumento){ //TODO devia estar a funciona
                 pos++;
             }
             atual = atual->next;
+            pos++;
         }
         cout << "Max Item " <<  max_val << " na posicao " << pos << "!\n" << endl;
     }
@@ -208,32 +221,91 @@ void ISll::find_max(string cmd, string argumento){ //TODO devia estar a funciona
 // 12. Remover um nó da posição da lista
 void ISll::delete_pos(string cmd, string argumentos){
     stringstream ssItems(argumentos);
-    int item;
-    if(verificarListaVazia(cmd) == false) {
-        INode* atual = head;
-        INode* anterior = nullptr;
+    int pos;
+    while(ssItems >> pos) {
+        if(verificarListaVazia(cmd) == false && pos>= 0 && pos < n ) {
+            /*INode* atual = head;
+            INode* anterior = nullptr;
 
-        while(ssItems >> item) {
-            while (atual != nullptr) {
-                if (atual->item == item) {
-                    if (anterior == nullptr) { // O valor está no primeiro nó
-                        head = atual->next;   // Avança
-                    } else {
-                        anterior->next = atual->next;// O valor não está no primeiro nó
+            while(ssItems >> item) {
+                while (atual != nullptr) {
+                    if (atual->item == item) {
+                        if (anterior == nullptr) { // O valor está no primeiro nó
+                            head = atual->next;   // Avança
+                        } else {
+                            anterior->next = atual->next;// O valor não está no primeiro nó
+                        }
+                        delete atual;
+                        return;
                     }
-                    delete atual;
-                    return;
+                    anterior = atual;
+                    atual = atual->next;
                 }
-                anterior = atual;
-                atual = atual->next;
+            }*/
+            INode *p = head;                // Ponteiro para percorrer a lista
+            INode *q = NULL;                // Ponteiro para guardar o nó anterior
+            int i = 0;                      // Índice para percorrer a lista
+
+            // Percorre a lista até chegar ao nó anterior ao nó que deve ser removido
+            while (i < pos) {
+                q = p;
+                p = p->next;
+                i++;
             }
+
+            // Atualiza o ponteiro do nó anterior para apontar para o nó seguinte do nó a ser removido
+            if (q == NULL) {                // Se o nó a ser removido for o primeiro da lista
+                head = p->next;
+            } else {
+                q->next = p->next;
+            }
+
+            delete p;                       // Remove o nó
+            n--;                            // Atualiza o tamanho da lista
+        }
         }
     }
- }
 
 // 13. Inverter a ordem dos itens da lista a partir da posição 1 até à posição indicada
 void ISll::invert_range(string cmd, string argumentos){ //TODO Atribuir oss valores paara cada posião e t
-    if(verificarListaVazia(cmd) == false) {
+    stringstream ssItems(argumentos);
+    int pos1;
+    int pos2;
+    while(ssItems >> pos1 >> pos2) {
+        if(verificarListaVazia(cmd) == false) {
+            if (pos1 > pos2) {  // Se a posição 1 é maior que a posição 2, troca as posições
+                int temp = pos1;
+                pos1 = pos2;
+                pos2 = temp;
+            }
+            INode *p1 = head;  // Criar ponteiro p1 e inicializá-lo para o head da lista
+            INode *prev1 = NULL;  // Criar ponteiro prev1 e inicializá-lo para NULL
+            for (int i = 1; i < pos1 && p1 != NULL; i++) { // Percorre a lista até a posição 1
+                prev1 = p1;  // Armazena o valor atual de p1 em prev1
+                p1 = p1->next; // Move o ponteiro p1 para o próximo nó da lista
+            }
+            INode *p2 = p1; // Criar ponteiro p2 e inicializá-lo para p1
+            INode *prev2 = prev1; // Criar ponteiro prev2 e inicializá-lo para prev1
+            for (int i = pos1; i < pos2 && p2 != NULL; i++) { // Percorre a lista até a posição 2
+                prev2 = p2;  // Armazena o valor atual de p2 em prev2
+                p2 = p2->next;  // Move o ponteiro p2 para o próximo nó da lista
+            }
+            if (p1 != NULL && p2 != NULL) {  // Se p1 e p2 forem diferentes de NULL
+                if (prev1 != NULL) {  // Se prev1 for diferente de NULL, atualiza o ponteiro next de prev1
+                    prev1->next = p2;
+                } else {  // Caso contrário, atualiza head para p2
+                    head = p2;
+                }
+                if (prev2 != NULL) {  // Se prev2 for diferente de NULL, atualiza o ponteiro next de prev2
+                    prev2->next = p1;
+                } else {  // Caso contrário, atualiza head para p1
+                    head = p1;
+                }
+                INode *temp = p2->next;  // Criar ponteiro temporário e inicializá-lo para o próximo nó após p2
+                p2->next = p1->next;  // Atualiza o ponteiro next de p2 para o próximo nó após p1
+                p1->next = temp;  // Atualiza o ponteiro next de p1 para o ponteiro temporário
+            }
+        }
 //        if (pos1 >= pos2 || pos1 < 0 || pos2 >= size) {
 //            cout << "Posicoes invalidas." << endl;
 //            return;
